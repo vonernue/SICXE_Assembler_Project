2c5beb3395aecda8f133208333ecdff2132e00c6
Vonernue <39663381+seanho12345@users.noreply.github.com>
2022-12-05 11:10:29 +0800

    put functions into utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 from termcolor import colored
 from utils.tables import *
 import os
+import sys
 
 if os.name == 'nt':
     os.system('color')
@@ -42,3 +43,16 @@ def printSymtab(symtab: dict):
         if key not in REGTAB.keys():
             print("{:<10} | {:<10}".format(key, hex(value)))
     print("-----------------------")
+
+def flagConstructor(n, i, x, b, p, e):
+    """
+    Construct the flags for the object code.
+    """
+    return n << 5 | i << 4 | x << 3 | b << 2 | p << 1 | e
+
+def twosComp(val, bytes):
+    """
+    Compute two's complement with specified bytes
+    """
+    b = val.to_bytes(bytes, byteorder=sys.byteorder, signed=True)   
+    return int.from_bytes(b, byteorder=sys.byteorder, signed=False)
